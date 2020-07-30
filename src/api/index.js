3096b136eed59c156c70545ceaec8da9736736b3
ksh030506 <ksh030506@naver.com>
2020-07-30 13:10:42 +0900

    ㅗ

--- src/api/index.js
+++ src/api/index.js
@@ -1,17 +1,17 @@
-import Router from 'koa-router';
+import Router from "koa-router";
 
-import auth from './auth';
-import credit from './credit';
-import question from './question';
-import sale from './sale';
-import trade from './trade';
+import auth from "./auth";
+import credit from "./credit";
+import question from "./question";
+import sale from "./sale";
+import trade from "./trade";
 
-const api = new Router;
+const api = new Router();
 
-api.use('/auth', auth.routes());
-api.use('/credit', credit.routes());
-api.use('/question', question.routes());
-api.use('/sale', sale.routes());
-api.use('/trade', trade.routes());
+api.use("/auth", auth.routes());
+api.use("/credit", credit.routes());
+api.use("/question", question.routes());
+api.use("/sale", sale.routes());
+api.use("/trade", trade.routes());
 
 export default api;
